0bde2e789a0cccc5dabc5c7f5974e4b7e61cf913
nohi <hyo15.3318@gmail.com>
2023-11-16 20:47:25 +0900

    11.16_20:46

---
+++ gitcomand.py
@@ -0,0 +1,10 @@
+# # git init
+# git add .
+# git commit -m ""
+# # git remote add origin https://github.com/no-hi/remote_nohi_11_16
+# # git push -u origin master
+# git push
+
+
+# git clone https://github.com/no-hi/remote_nohi_11_16
+# git pull
