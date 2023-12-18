4b18d240a9dbe769c59660d88330f6e732be53c6
nohi <hyo15.3318@gmail.com>
2023-12-19 03:18:30 +0900

    12.18

--- python最適化/GA_graph.py
+++ python最適化/GA_graph.py
@@ -3,84 +3,55 @@ import numpy as np
 
 ##############################################################################
 
-cost = [[[3083869.9778700015, 0, 329899.8818864615, 138135.64307217477, 0, 0], [2201519.0800500005, 0, 342047.53477158566, 162512.92984408402, 0, 0], [1402573.0722900003, 0, 355771.4966967819, 205841.14094314096, 0, 0], [1038362.30988, 0, 370066.5367502598, 250846.51253793284, 0, 0], [831602.9737199997, 0, 383553.5011646385, 288363.6782075949, 0, 0], [646492.7377499996, 0, 397615.02799237816, 345332.6310899478, 0, 0], [510710.41811999993, 0, 407908.6272932413, 382365.82904059737, 0, 0]], [[2272390.8020699993, 198615.18181799995, 329899.8818864615, 138135.64307217477, 7653.720155424562, 538239.5382205871], [1369567.9353300005, 198615.18181799995, 342047.53477158566, 162512.92984408402, 7653.720155424562, 538239.5382205871], [1308970.0427400002, 28916.680626000005, 355856.3595257742, 206581.6678337846, 1158.9205813269793, 62306.97036175476], [1034909.40132, 1634.53944, 370066.5367502598, 250846.51253793284, 160.9236516894888, 7062.95509722004], [832710.2621699999, 0, 384110.53616400325, 307426.8108801027, 0, 0], [644601.1631999996, 630.2405100000001, 397615.02799237816, 345332.6310899478, 139.51646926533232, 6352.544853535483], [510330.9901199999, 20000.60496000000003, 407908.6272932413, 382365.82904059737, 50000.12293421063784, 20000.766374232437]]]
+cost = [[[3083869.9778700015, 0, 329899.8818864615, 138135.64307217477, 0, 0], [2214929.7922499995, 0, 342285.3404406706, 164599.8166212692, 0, 0], [1402573.0722900003, 0, 355771.4966967819, 205841.14094314096, 0, 0], [1038362.30988, 0, 370066.5367502598, 250846.51253793284, 0, 0]], [[2272390.8020699993, 198615.18181799995, 329899.8818864615, 138135.64307217477, 7653.720155424562, 538239.5382205871], [1369567.9353300005, 198615.18181799995, 342047.53477158566, 162512.92984408402, 7653.720155424562, 538239.5382205871], [1308970.0427400002, 28916.680626000005, 355856.3595257742, 206581.6678337846, 1158.9205813269793, 62306.97036175476], [1034909.40132, 1634.53944, 370066.5367502598, 250846.51253793284, 160.9236516894888, 7062.95509722004]], [[2272390.8020699993, 187946.97055799994, 329899.8818864615, 138135.64307217477, 7999.965117319442, 538939.5520703855], [1369567.9353300005, 185691.14306999993, 342047.53477158566, 162512.92984408402, 7999.965117319442, 538939.5520703855], [1307825.1947400002, 27119.717958000005, 355755.4651551349, 205701.72484356182, 1293.6328783928393, 64418.93247901377], [974770.5225599998, 16419.270204, 370066.5367502598, 250846.51253793284, 1186.4673276463186, 58750.21216883008]], [[1347279.43365, 533426.789136, 329899.8818864615, 138135.64307217477, 17884.678670683476, 1222379.145026599], [1369567.9353300005, 181477.98696599997, 342047.53477158566, 162512.92984408402, 8317.114185350329, 541039.5647563483], [1304718.16293, 28448.719152000005, 355755.4651551349, 205701.72484356182, 1521.6665475015454, 74316.49444036053], [954105.1122299999, 15602.002788000002, 369920.74918130506, 249747.23673534326, 1261.2843783045187, 62975.324291923076]], [[910911.1912499999, 586692.780966, 329899.8818864615, 138135.64307217477, 22837.326652547505, 1553089.038252518], [1369567.9353300005, 178984.04955599998, 342047.53477158566, 162512.92984408402, 8611.626660741482, 542439.5765368473], [1291079.6443500002, 36781.41790200001, 355651.83999979426, 204803.98420990244, 1694.6355468740219, 84207.58349884654], [953931.8412299999, 15584.176818000004, 369920.74918130506, 249747.23673534326, 1290.4988358194166, 65077.927807350694]], [[1189450.6380599996, 589364.1655979999, 329899.8818864615, 138135.64307217477, 18736.372571026168, 1237211.1954466838], [1369567.9353300005, 177613.63546199998, 342047.53477158566, 162512.92984408402, 8684.144457818873, 543139.5794375592], [1291079.6443500002, 36768.79065000001, 355651.83999979426, 204803.98420990244, 1702.7122816795388, 87007.58382191592], [970480.24134, 16933.583298, 370066.5367502598, 250846.51253793284, 1410.5095015867778, 70721.89116942116]]]
 horizon = "inc"
 # horizon = "trans"
 
-
 ##############################################################################
+
+def bar_chart(cost_):
+    TC_direct = cost_[:, 0]
+    TC_indirect = cost_[:, 1]
+    IC_inc = cost_[:, 2]
+    OC_inc = cost_[:, 3]
+    IC_trans = cost_[:, 4]
+    OC_trans = cost_[:, 5]
+
+    plt.figure(figsize=(12, 6))
+    plt.bar(N_INC, TC_direct, color='blue', label='TC_direct')
+    plt.bar(N_INC, TC_indirect, bottom=TC_direct, color='blue', linestyle='--', label='TC_indirect')
+    plt.bar(N_INC, IC_inc, bottom=TC_direct + TC_indirect, color='orange', label='IC_inc')
+    plt.bar(N_INC, OC_inc, bottom=TC_direct + TC_indirect + IC_inc, color='orange', linestyle='--', label='OC_inc')
+    plt.bar(N_INC, IC_trans, bottom=TC_direct + TC_indirect + IC_inc + OC_inc, color='yellow', label='IC_trans')
+    plt.bar(N_INC, OC_trans, bottom=TC_direct + TC_indirect + IC_inc + OC_inc + IC_trans, color='yellow', linestyle='--', label='OC_trans')
+
+    # 同色間点線の描画
+    for x in N_INC:
+        plt.hlines(TC_direct[x], x - 0.4, x + 0.4, colors='black', linestyles='dotted')
+        plt.hlines(TC_direct[x] + TC_indirect[x] + IC_inc[x], x - 0.4, x + 0.4, colors='black', linestyles='dotted')
+        plt.hlines(TC_direct[x] + TC_indirect[x] + IC_inc[x] + OC_inc[x] + IC_trans[x], x - 0.4, x + 0.4, colors='black', linestyles='dotted')
+
+    # 凡例
+    handles, labels = plt.gca().get_legend_handles_labels()
+    # plt.legend(handles[::-1], labels[::-1], loc='upper left', bbox_to_anchor=(1.05, 1))        
+    plt.legend(handles[::-1], labels[::-1], loc='upper left', bbox_to_anchor=(1, 1))        
+
+    # ラベル
+    plt.xlabel('N_INC')
+    plt.ylabel('Cost')
+    plt.title(f'Stacked Bar Chart for N_TRANS = {N_TRANS}')
+    plt.tight_layout(rect=[0, 0, 0.85, 1])
+    plt.show()
+
 if horizon == "inc":
     for N_TRANS in range(len(cost)):
-        current_data = np.array(cost[N_TRANS])
-        N_INC = np.arange(current_data.shape[0])
-
-        TC_direct = current_data[:, 0]
-        TC_indirect = current_data[:, 1]
-        IC_inc = current_data[:, 2]
-        OC_inc = current_data[:, 3]
-        IC_trans = current_data[:, 4]
-        OC_trans = current_data[:, 5]
-
-        plt.figure(figsize=(12, 6))
-        plt.bar(N_INC, TC_direct, color='blue', label='TC_direct')
-        plt.bar(N_INC, TC_indirect, bottom=TC_direct, color='blue', linestyle='--', label='TC_indirect')
-        plt.bar(N_INC, IC_inc, bottom=TC_direct + TC_indirect, color='orange', label='IC_inc')
-        plt.bar(N_INC, OC_inc, bottom=TC_direct + TC_indirect + IC_inc, color='orange', linestyle='--', label='OC_inc')
-        plt.bar(N_INC, IC_trans, bottom=TC_direct + TC_indirect + IC_inc + OC_inc, color='yellow', label='IC_trans')
-        plt.bar(N_INC, OC_trans, bottom=TC_direct + TC_indirect + IC_inc + OC_inc + IC_trans, color='yellow', linestyle='--', label='OC_trans')
-
-        # 同色間点線の描画
-        for x in N_INC:
-            plt.hlines(TC_direct[x], x - 0.4, x + 0.4, colors='black', linestyles='dotted')
-            plt.hlines(TC_direct[x] + TC_indirect[x] + IC_inc[x], x - 0.4, x + 0.4, colors='black', linestyles='dotted')
-            plt.hlines(TC_direct[x] + TC_indirect[x] + IC_inc[x] + OC_inc[x] + IC_trans[x], x - 0.4, x + 0.4, colors='black', linestyles='dotted')
-
-        # 凡例
-        plt.legend(loc='upper right', bbox_to_anchor=(1, 1))
-        
-        # ラベル
-        plt.xlabel('N_INC')
-        plt.ylabel('Cost')
-        plt.title(f'Stacked Bar Chart for N_TRANS = {N_TRANS}')
-        plt.tight_layout(rect=[0, 0, 0.85, 1])
-        plt.show()
+        cost_per_inc = np.array(cost[N_TRANS])
+        N_INC = np.arange(cost_per_inc.shape[0])
+        bar_chart(cost_per_inc)
 
 if horizon == "trans":
-    # コストデータの転置処理
-    transposed_cost = np.transpose(cost, (2, 0, 1))
-
-    # 積み上げ棒グラフの描画
+    transposed_cost = np.transpose(cost, (1, 0, 2))
     for N_INC in range(len(transposed_cost)):
-        current_data = np.array(transposed_cost[N_INC])
-        N_TRANS = np.arange(current_data.shape[0])
-
-        TC_direct = current_data[:, 0]
-        TC_indirect = current_data[:, 1]
-        IC_inc = current_data[:, 2]
-        OC_inc = current_data[:, 3]
-        IC_trans = current_data[:, 4]
-        OC_trans = current_data[:, 5]
-
-        plt.figure(figsize=(12, 6))
-        plt.bar(N_TRANS, TC_direct, color='blue', label='TC_direct')
-        plt.bar(N_TRANS, TC_indirect, bottom=TC_direct, color='blue', linestyle='--', label='TC_indirect')
-        plt.bar(N_TRANS, IC_inc, bottom=TC_direct + TC_indirect, color='orange', label='IC_inc')
-        plt.bar(N_TRANS, OC_inc, bottom=TC_direct + TC_indirect + IC_inc, color='orange', linestyle='--', label='OC_inc')
-        plt.bar(N_TRANS, IC_trans, bottom=TC_direct + TC_indirect + IC_inc + OC_inc, color='yellow', label='IC_trans')
-        plt.bar(N_TRANS, OC_trans, bottom=TC_direct + TC_indirect + IC_inc + OC_inc + IC_trans, color='yellow', linestyle='--', label='OC_trans')
-
-        # 同色間点線の描画
-        for x in N_TRANS:
-            plt.hlines(TC_direct[x], x - 0.4, x + 0.4, colors='black', linestyles='dotted')
-            plt.hlines(TC_direct[x] + TC_indirect[x] + IC_inc[x], x - 0.4, x + 0.4, colors='black', linestyles='dotted')
-            plt.hlines(TC_direct[x] + TC_indirect[x] + IC_inc[x] + OC_inc[x] + IC_trans[x], x - 0.4, x + 0.4, colors='black', linestyles='dotted')
-
-        # 凡例
-        plt.legend(loc='upper right', bbox_to_anchor=(1, 1))
-        
-        # ラベル
-        plt.xlabel('N_TRANS')
-        plt.ylabel('Cost')
-        plt.title(f'Stacked Bar Chart for N_INC = {N_INC}')
-        plt.tight_layout(rect=[0, 0, 0.85, 1])
-        plt.show()
+        cost_per_trans = np.array(transposed_cost[N_INC])
+        N_TRANS = np.arange(cost_per_trans.shape[0])
+        bar_chart(cost_per_trans)
