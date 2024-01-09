06106335f20d43b20a7eebf1949c030b270f3dd1
nohi <hyo15.3318@gmail.com>
2024-01-09 14:36:26 +0900

    1.9

--- gitcomand.py
+++ gitcomand.py
@@ -11,10 +11,18 @@
 
 
 # git pull
-# git reset --hard origin/master        無理やりpull
 
+# 無理やりpull
+# git reset --hard origin/master
+
+# 一時保存してマージ
 # git fetch               最新の変更を取得する
 # git stash               現在の変更を一時保存
 # git merge origin/main   リモートの変更をマージする
 # git pull
 # git stash pop           スタッシュからローカルの変更を戻す
+
+# このＰＣでの変更を破棄してpull
+# git checkout -- python最適化/GA中継_トナメ_kanen.py
+# git checkout -- python最適化/GA中継_トナメ_sanpai.py
+# git pull
