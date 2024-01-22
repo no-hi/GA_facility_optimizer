b9e879dfe252cf2dacceb306b2a68fedd52bb5c2
nohi <hyo15.3318@gmail.com>
2024-01-22 15:32:53 +0900

    自動コミット中途:877kanen1~1&0~1

--- python最適化/GA中継_並列master.py
+++ python最適化/GA中継_並列master.py
@@ -630,8 +630,8 @@ def GA_optimization(N_INC, N_TRANS, output_directory, current_time, lock, lock2,
             # 時点N_INC以下のデータのみを抽出
             filtered_cost_2D = normal_cost_2D[:N_INC]
             with open(os.path.join(output_directory, f"GA_Graph({UNIT_TRANS}{waste_name}){current_time}.txt"), 'w', encoding="utf-8") as file:
-                file.write(f"#inc({N_INC_INITIAL}~{N_INC})+trans({N_TRANS_INITIAL}~{N_TRANS_MAX})コスト行列\n")
-                file.write(f"foldername = '{str(waste_name)}{str(UNIT_TRANS)}'\n")
+                file.write(f"info_inc=[{N_INC_INITIAL},{N_INC}]\ninfo_trans=[{N_TRANS_INITIAL}~{N_TRANS_MAX}]\n")
+                file.write(f'foldername = "{str(waste_name)}{str(UNIT_TRANS)}"\n')
                 file.write(f"cost = {str(filtered_cost_2D)}\n")
             # 自動git pull/push
             all_conditions_met = all(counter[i] == N_TRANS_MAX - N_TRANS_INITIAL + 1 for i in range(N_INC_INITIAL, N_INC + 1))
