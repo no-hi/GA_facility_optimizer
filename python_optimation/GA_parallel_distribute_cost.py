436524dac7900de32ca3447e76d9bced044a5986
nohi <hyo15.3318@gmail.com>
2024-01-31 13:35:48 +0900

    自動コミット（every_INC）:877kanen1~1&0~1

--- python_optimation/GA_parallel_distribute_cost.py
+++ python_optimation/GA_parallel_distribute_cost.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
         
         if restarting_output_directory == "":
             output_directory_name = f"{UNIT_TRANS}{waste_name}{N_INC_INITIAL}~{N_INC_MAX}&{N_TRANS_INITIAL}~{N_TRANS_MAX}{add_name}_{current_time}"
-            output_directory = os.path.join(current_directory, "output_cost", output_directory_name)
+            output_directory = os.path.join(current_directory, "GA__output_cost", output_directory_name)
             if not os.path.exists(output_directory):
                 os.makedirs(output_directory)
 
@@ -77,7 +77,7 @@ if __name__ == '__main__':
         
         else:  # 中断入力時の再開
             output_directory_name = restarting_output_directory
-            output_directory = os.path.join(current_directory, "output_cost", output_directory_name)
+            output_directory = os.path.join(current_directory, "GA__output_cost", output_directory_name)
             if not os.path.exists(output_directory):
                 print(f"指定された中断フォルダが存在しません。")
                 sys.exit(1)
