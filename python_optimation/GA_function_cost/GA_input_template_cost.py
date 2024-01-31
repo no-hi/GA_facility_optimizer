b6df6174e5f5319cbf4600f74d9103b6d78097ab
nohi <hyo15.3318@gmail.com>
2024-01-31 16:28:48 +0900

    1.31

--- python_optimation/GA_function_cost/GA_input_template_cost.py
+++ python_optimation/GA_function_cost/GA_input_template_cost.py
@@ -1,20 +1,18 @@
-import data
+import data as data
 hokkaido = data.name
 # TOP_N_CITIES = N_INC + N_TRANS +10          # ごみ量順位下限→ループ内で設定
-
-# 再開したい中断フォルダ名
-restarting_output_directory = ""
 
 add_name = ""
 waste_name = "kanen"
 # waste_name = "sanpai"
 N_CITIES = len(hokkaido)   # 市町村数
+restarting_output_directory = ""    # 再開したい中断フォルダ名
 N_INC_INITIAL = 1          # 焼却初期値
-N_INC_MAX = 10             # 焼却上限
+N_INC_MAX = 3             # 焼却上限
 N_TRANS_INITIAL = 0        # 中継初期値
-N_TRANS_MAX = 10           # 中継上限
+N_TRANS_MAX = 1           # 中継上限
 N_IND_UNIT = 50            # 1施設当たり個体数
-N_GEN = 1000               # 世代数
+N_GEN = 10               # 世代数
 CX_PROB = 0.7              # 一様交叉
 MUT_PROB = 0.3             # 突然変異
 TOUR_SIZE = 4              # トーナメント
