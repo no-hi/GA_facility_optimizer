19514768667631f687f56273d60f9ff61556b012
nohi <hyo15.3318@gmail.com>
2024-02-01 13:30:32 +0900

    2.1

---
+++ python_optimization/class_individual.py
@@ -0,0 +1,20 @@
+class Fitness:
+    def __init__(self):
+        self.values = []
+
+class Individual:
+    def __init__(self):
+        self.inc_facility = []
+        self.trans_facility = []
+        self.unused_cities = []
+        self.fitness = Fitness()
+
+# 使用例
+individual = Individual()
+individual.inc_facility = [1, 2, 3]
+individual.trans_facility = [4, 5, 6]
+individual.unused_cities = [7, 8, 9]
+individual.fitness.values = [10, 11, 12]
+
+# individualの属性を表示
+(individual.inc_facility, individual.trans_facility, individual.unused_cities, individual.fitness.values)
