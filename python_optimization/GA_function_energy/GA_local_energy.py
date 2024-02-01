f437ba5598096f2bf67f83f4f435ad44fd3d7b0d
nohi <hyo15.3318@gmail.com>
2024-02-01 23:46:52 +0900

    自動コミット(エネルギースタート):4.8kanen7~9&0~0

--- python_optimization/GA_function_energy/GA_local_energy.py
+++ python_optimization/GA_function_energy/GA_local_energy.py
@@ -4,35 +4,34 @@ import GA_function_energy.GA_input_energy as input
 def local_optimization(best_individual, total_energy):
     current_best = best_individual
     current_best_score, *_ = total_energy(best_individual)
-    
-    print(best_individual.inc_facility, best_individual.trans_facility )
-    
-    # 遺伝子全通り計算
-    fixed_indices = set()
-    while True:
-        new_best_found = False
-        facilities = current_best.inc_facility + current_best.trans_facility
-        for i in range(len(facilities)):
-            if i in fixed_indices:
-                continue  # 変更された遺伝子位置はスキップ
-            for new_facility in range(input.N_CITIES):
-                print(facilities[:i] + facilities[i+1:])
-                if new_facility in facilities[:i] + facilities[i+1:]:
-                    continue
-                individual = deepcopy(current_best)
-                if i < len(current_best.inc_facility):
-                    individual.inc_facility[i] = new_facility
-                else:
-                    individual.trans_facility[i - len(current_best.inc_facility)] = new_facility
+        
+    with open('print.txt', 'w') as file:
+        # 遺伝子全通り計算
+        fixed_indices = set()
+        while True:
+            new_best_found = False
+            facilities = current_best.inc_facility + current_best.trans_facility
+            for i in range(len(facilities)):
+                if i in fixed_indices:
+                    continue  # 変更された遺伝子位置はスキップ
+                for new_facility in range(input.N_CITIES):
+                    print(facilities[:i] + facilities[i+1:])
+                    if new_facility in facilities[:i] + facilities[i+1:]:
+                        continue
+                    individual = deepcopy(current_best)
+                    if i < len(current_best.inc_facility):
+                        individual.inc_facility[i] = new_facility
+                    else:
+                        individual.trans_facility[i - len(current_best.inc_facility)] = new_facility
 
-                score, *_ = total_energy(individual)
-                if score < current_best_score:
-                    current_best = individual
-                    current_best_score = score
-                    new_best_found = True
-                    fixed_indices.add(i)  # 新しい最適遺伝子を固定
-        if not new_best_found:
-            break
+                    score, *_ = total_energy(individual)
+                    if score < current_best_score:
+                        current_best = individual
+                        current_best_score = score
+                        new_best_found = True
+                        fixed_indices.add(i)  # 新しい最適遺伝子を固定
+            if not new_best_found:
+                break
     
     
     # 最適個体の属性修正
