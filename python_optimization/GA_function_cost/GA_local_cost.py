18327a46c60a147a633fba98a2147c1c7845a407
nohi <hyo15.3318@gmail.com>
2024-02-02 01:14:12 +0900

    2.2

--- python_optimization/GA_function_cost/GA_local_cost.py
+++ python_optimization/GA_function_cost/GA_local_cost.py
@@ -4,12 +4,15 @@ import GA_function_cost.GA_input_cost as input
 def local_optimization(best_individual, total_cost):
     current_best = best_individual
     current_best_score, *_ = total_cost(best_individual)
-    
+        
     # 遺伝子全通り計算
+    fixed_indices = set()
     while True:
         new_best_found = False
         facilities = current_best.inc_facility + current_best.trans_facility
         for i in range(len(facilities)):
+            if i in fixed_indices:
+                continue  # 変更された遺伝子位置はスキップ
             for new_facility in range(input.N_CITIES):
                 if new_facility in facilities[:i] + facilities[i+1:]:
                     continue
@@ -23,10 +26,14 @@ def local_optimization(best_individual, total_cost):
                 if score < current_best_score:
                     current_best = individual
                     current_best_score = score
+                    if i < len(current_best.inc_facility):
+                        facilities[i] = new_facility
+                    else:
+                        facilities[i - len(current_best.inc_facility)] = new_facility
                     new_best_found = True
+                    fixed_indices.add(i)  # 新しい最適遺伝子を固定
         if not new_best_found:
             break
-    
     
     # 最適個体の属性修正
     new_individual = deepcopy(best_individual)
@@ -34,6 +41,8 @@ def local_optimization(best_individual, total_cost):
     new_individual.trans_facility = current_best.trans_facility
     new_individual.unused_cities = set(range(input.N_CITIES)) - set(new_individual.inc_facility) - set(new_individual.trans_facility)
     new_individual.fitness.values = [current_best_score]
-    
-    
+        
+        
     return new_individual
+
+
