8acc63ecc875b5147298283c04096c08a0a00a91
nohi <hyo15.3318@gmail.com>
2024-02-02 00:02:18 +0900

    自動コミット(エネルギースタート):4.8kanen9~9&0~0

--- python_optimization/GA_function_energy/GA_local_energy.py
+++ python_optimization/GA_function_energy/GA_local_energy.py
@@ -11,18 +11,20 @@ def local_optimization(best_individual, total_energy):
         while True:
             new_best_found = False
             facilities = current_best.inc_facility + current_best.trans_facility
+            print("Current facilities:", facilities, file=file)  # 現在のfacilitiesの状態を出力
             for i in range(len(facilities)):
                 if i in fixed_indices:
                     continue  # 変更された遺伝子位置はスキップ
                 for new_facility in range(input.N_CITIES):
-                    print(facilities[:i] + facilities[i+1:], file=file)
                     if new_facility in facilities[:i] + facilities[i+1:]:
                         continue
                     individual = deepcopy(current_best)
+                    print("Before change:", individual.inc_facility, individual.trans_facility, file=file)  # 変更前のindividualの状態を出力
                     if i < len(current_best.inc_facility):
                         individual.inc_facility[i] = new_facility
                     else:
                         individual.trans_facility[i - len(current_best.inc_facility)] = new_facility
+                    print("After change:", individual.inc_facility, individual.trans_facility, file=file)  # 変更後のindividualの状態を出力
 
                     score, *_ = total_energy(individual)
                     if score < current_best_score:
@@ -30,9 +32,10 @@ def local_optimization(best_individual, total_energy):
                         current_best_score = score
                         new_best_found = True
                         fixed_indices.add(i)  # 新しい最適遺伝子を固定
+                        print("Updated current_best:", current_best.inc_facility, current_best.trans_facility, file=file)  # 更新後のcurrent_bestの状態を出力
             if not new_best_found:
                 break
-    
+            print("Fixed indices:", fixed_indices, file=file)  # fixed_indicesの状態を出力
     
     # 最適個体の属性修正
     new_individual = deepcopy(best_individual)
@@ -41,9 +44,6 @@ def local_optimization(best_individual, total_energy):
     new_individual.unused_cities = set(range(input.N_CITIES)) - set(new_individual.inc_facility) - set(new_individual.trans_facility)
     new_individual.fitness.values = [current_best_score]
     
-    print(new_individual.inc_facility, new_individual.trans_facility )
-    
+    print("Final new_individual:", new_individual.inc_facility, new_individual.trans_facility, file=file)  # 最終的なnew_individualの状態を出力
     
     return new_individual
-
-    
